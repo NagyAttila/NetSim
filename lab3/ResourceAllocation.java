ceb4ef554d8eef5b06f5001835574b3025d1bcbb
lorcs1 <attilanagy85@gmail.com>
2012-11-21 16:13:23 +0100

    Request send is implemented.

--- lab3/ResourceAllocation.java
+++ lab3/ResourceAllocation.java
@@ -1,9 +1,16 @@
 package lab3;
 
+
 import netsim.protocol.*;
+import java.util.Hashtable;
+import java.util.PriorityQueue;
 
 public class ResourceAllocation extends netsim.protocol.ProtocolAdapter
 {
+  private int clock;
+  private Hashtable timestamps = new Hashtable();
+  private PriorityQueue<Request> requests = new PriorityQueue<Request>();
+
   public String toString()
   {
     return "ResourceAllocation";
@@ -11,27 +18,31 @@ public class ResourceAllocation extends netsim.protocol.ProtocolAdapter
 
   protected void receiveMessage(Message message, InLink inLink) throws Exception
   {
+    MyMessage msg = (MyMessage)message;
     myNode.setWaken();
+    requests.add( msg.request );
+    myNode.writeLogg("HEAD:" + requests.peek().time);
   }
 
   public void trigg() throws Exception
   {
-    myNode.sendToAllOutlinks(new MyMessage(myNodeName, "Pa aterseende!"));
+    myNode.sendToAllOutlinks(new MyMessage(myNodeName, new Request(clock,myNodeName)));
+    clock++;
   }
 
   private class MyMessage implements Message
   {
     String sender;
-    String message;
-    private MyMessage(String sender, String message)
+    Request request;
+    private MyMessage(String sender, Request request)
     {
       this.sender = sender;
-      this.message = message;
+      this.request = request;
     }
 
     public Message clone()
     {
-      return new MyMessage(sender, message);
+      return new MyMessage(sender, request);
     }
 
     public String getTag()
@@ -39,4 +50,24 @@ public class ResourceAllocation extends netsim.protocol.ProtocolAdapter
       return sender;
     }
   }
+
+  private class Request implements Comparable<Request>
+  {
+    private Request(int time, String sender)
+    {
+      this.time = time;
+      this.sender = sender;
+    }
+
+    public int compareTo(Request req)
+    {
+      return this.time - req.time;
+    }
+    
+    int time;
+    String sender;
+  }
+
 }
+
+
